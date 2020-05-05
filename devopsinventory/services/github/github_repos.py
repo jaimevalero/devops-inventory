22aaea583f95459a85543aed19b9d2fd727099b6
Jaime Valero <jaimevalero78@yahoo.es>
2020-05-05 10:17:49 +0200

    Update github_repos.py

--- devopsinventory/services/github/github_repos.py
+++ devopsinventory/services/github/github_repos.py
@@ -5,12 +5,19 @@ from devopsinventory.base.abstract_service import Service
 
 
 class GitHubPuller(worker.Puller):
-    pass
-    #def register(self, worker):
+    """ Example class, to get github repos of an organization"""
+    
+    @run
+    def get_repos(config):
+        # TO-DO request, etc.
+        # return df
+        pass
+    
 
 
 
 if __name__ == "__main__":
     print('Ejecutando como programa this')
-    sev = Service(config={})
-    sev.register(GitHubPuller(config={}))
+    service = Service(config={})
+    service.register(GitHubPuller(config={}))
+    service.run()
